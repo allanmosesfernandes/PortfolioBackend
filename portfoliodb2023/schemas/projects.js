da37b8b8accb805822a044ba907cde9d32fe1a4e
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-26 13:44:52 +0100

    Project-Schema

--- portfoliodb2023/schemas/projects.js
+++ portfoliodb2023/schemas/projects.js
@@ -12,7 +12,7 @@ export default defineType({
       description: 'Year of the project',
       //add validation so date is not greater than current date
       //with custom error message year cannot be greater than current year
-        validation: Rule => Rule.max(new Date().getFullYear()).error('Please enter valid date')
+        validation: Rule => Rule.required()
     }),
     defineField({
         name: 'Title',
@@ -20,5 +20,23 @@ export default defineType({
         type: 'string',
         description: 'Title of the project',
         validation: Rule => Rule.required().min(10).max(50)
+    }),
+    //Built with
+    defineField({
+        name: 'BuiltWith',
+        title: 'Built With',
+        type: 'array',
+        of: [{type: 'string'}],
+        description: 'Technologies used to build the project',
+        validation: Rule => Rule.required().min(1).max(10)
+    }),
+    //Project Link
+    defineField({
+        name: 'ProjectLink',
+        title: 'Project Link',
+        type: 'url',
+        description: 'Link to the project',
+        validation: Rule => Rule.required().uri()
+    }),
   ],
 }) 
